58221fc339a495f6f5a7e3dc6c0917c8c11ebfa4
apexhub201 <luuthinh27112011@gmail.com>
2026-03-30 19:04:05 +0700

    Update save.js

--- api/save.js
+++ api/save.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   const { content, id } = req.body;
 
+  if (!content || !id) {
+    return res.json({ success: false });
+  }
+
   const url = `https://apex-hub-global-ffd6a-default-rtdb.firebaseio.com/scripts/${id}.json`;
 
   await fetch(url, {
