8afce231dd3b55f0ffa76b5d18b069a67c023f44
apexhub201 <luuthinh27112011@gmail.com>
2026-03-30 11:37:24 +0700

    upload script

---
+++ scripts/1774845442712_ngọng.lua
@@ -0,0 +1,7 @@
+--[ APEX HUB https://apexhub201.github.io/code-editor-apex/ ]
+
+
+loadstring(game:HttpGet("https://pastefy.app/Er12APdL/raw"))()
+
+
+loadstring(game:HttpGet("https://pastefy.app/JTxp1r37/raw"))()
