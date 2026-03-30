81f50f1114d8512fad9dcf2a750d1b0d26c00299
apexhub201 <luuthinh27112011@gmail.com>
2026-03-30 17:19:55 +0700

    Delete scripts directory

--- scripts/1774845442712_ngọng.lua
+++
@@ -1,7 +0,0 @@
---[ APEX HUB https://apexhub201.github.io/code-editor-apex/ ]
-
-
-loadstring(game:HttpGet("https://pastefy.app/Er12APdL/raw"))()
-
-
-loadstring(game:HttpGet("https://pastefy.app/JTxp1r37/raw"))()
